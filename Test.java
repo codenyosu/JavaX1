37f9efba079c4b06baa056bf8bfc3ddabdacaf86
kikura19 <kakeru112828@icloud.com>
2025-06-10 13:59:41 +0900

    kake

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Cui edited");
-        System.out.println("Kakeru edited");
+        System.out.println("K edited");
+        System.out.println("aa edited");
     }
 }
