f9cefcc396a94ea2b1e0a565ee465dc732fd9b70
Mai Yamamoto <maiyamamoto2002@gmail.com>
2025-06-10 14:01:59 +0900

    いじいじ山本

--- Test.java
+++ Test.java
@@ -2,5 +2,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Cui edited");
         System.out.println("Kakeru edited");
+
+        String tsuchi = "天才や～";
+        System.out.println(tsuchi);
     }
 }
