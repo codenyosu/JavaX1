1328aca97e5511e39cf7525a529459edc2838d78
asterkaka <yuejiapan.00@gmail.com>
2025-06-10 13:27:39 +0900

    えつかが編集したよ

--- etsuka.java
+++ etsuka.java
@@ -3,5 +3,6 @@ public class etsuka {
     public static void main(String[] args) {
         System.out.println("眠い");
         System.out.println("休みたい");
+        System.out.println("何が好き？");
     }
 }
